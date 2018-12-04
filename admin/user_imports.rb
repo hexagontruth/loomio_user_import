7c1585b2bafdf2af0820d97b4a01ce1da86245aa
Graham Steele <graham@hexnet.org>
2018-12-03 21:49:23 -0500

    Add group membership handling

--- admin/user_imports.rb
+++ admin/user_imports.rb
@@ -9,11 +9,25 @@ ActiveAdmin.register_page 'User Import' do
   page_action :import_csv, method: :post do
     contents = params[:import_csv][:file].read
     imported = 0
+    groups = {}
 
     CSV.parse(contents, headers: true).each do |row|
       next if (User.find_by(email: row.field('email')))
-      result = User.create(row.to_h)
-      imported += 1 if result
+      attrs = row.to_h
+      user_groups = attrs.delete('groups')&.split(',') || []
+      user = User.create(attrs) or next
+      imported += 1
+      if !user_groups.empty?
+        user_groups.each do |group_name|
+          if !groups[group_name]
+            groups[group_name] = Group.find_by(name: group_name) || :invalid
+          end
+          if groups[group_name].is_a? Group
+            user.groups.push(groups[group_name])
+          end
+        end
+        user&.save
+      end
     end
 
     redirect_to admin_user_import_path, notice: "Created #{imported} new users"
